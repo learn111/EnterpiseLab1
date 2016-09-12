c749f793f9ebcc3fa15de37c1c08701f907941f2
s.sapozhnikov <core1708@gmail.com>
2016-09-12 10:35:16 +0600

    initial

---
+++ CommonContracts/BaseCommonContract.cs
@@ -0,0 +1,13 @@
+﻿using DalContracts;
+
+namespace CommonContracts
+{
+    public abstract class BaseCommonContract
+    {
+        public abstract string ErrorMessage { get; set; }
+
+        public abstract string ErrorDetail { get; set; }
+
+        public abstract IProcedureMapper GetMapper();
+    }
+}
---
+++ DalContracts/IProcedureMapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DalContracts
+{
+    public interface IProcedureMapper
+    {
+        KeyValuePair<string, List<string>> GetMapByKey(Enum key);
+    }
+}
